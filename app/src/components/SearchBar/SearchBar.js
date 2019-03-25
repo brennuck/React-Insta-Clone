315f5532ed0984d82e64a86e0dee8b55914d2134
Brennon Nuckols <brennuck@gmail.com>
2019-03-25 14:43:58 -0600

    Added search bar content

---
+++ app/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const SearchBar = () => {
+    return (
+        <div className="search-header">
+            <div className="items">
+                <i class="fab fa-instagram"></i>
+                <h1>Instagram</h1>
+                <input placeholder="Search"/>
+                <i class="far fa-compass"></i>
+                <i class="far fa-heart"></i>
+                <i class="far fa-user"></i>
+            </div>
+        </div>
+    )
+}
+
+export default SearchBar;
